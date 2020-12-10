f4f3c4937eae8d98010a83ec542e0fe425af6802
Shoeb <Shoebmah91@gmail.com>
2020-12-10 18:49:15 -0500

    t

--- DecemebeTddProject/src/test/java/com/ups/testrunner/UpsTracking.java
+++ DecemebeTddProject/src/test/java/com/ups/testrunner/UpsTracking.java
@@ -2,6 +2,7 @@ package com.ups.testrunner;
 
 import java.util.concurrent.TimeUnit;
 
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,22 +11,40 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class UpsTracking {
 
 	public static void main(String[] args) throws InterruptedException {
-	WebDriver driver=new ChromeDriver();
-	System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-	driver.get("https://www.ups.com/track?loc=en_US&requester=ST/");
-	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(1,TimeUnit.SECONDS );
-	WebElement Track;
-	WebElement Cookies;
-	WebElement Errormsg;
+		
 	
+		
 	
-	Track=driver.findElement(By.xpath(//*[@id="stApp_btnTrack"]))
+		WebDriver chromeDriver = new ChromeDriver(); //Upcasting
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		chromeDriver.get("https://www.ups.com/lasso/login");
+		chromeDriver.manage().window().maximize();
+		chromeDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		
+		WebElement userID  = chromeDriver.findElement(By.name("userID"));
+		WebElement password = chromeDriver.findElement(By.xpath("//*[@id='pwd']"));
+		WebElement loginButton = chromeDriver.findElement(By.id("submitBtn"));
+		
+		
 	
+		
+		userID.sendKeys("WER");
+		Thread.sleep(2000);
+		password.sendKeys("password123456789");
+		Thread.sleep(2000);
+		loginButton.click();
+		Thread.sleep(2000);
+		chromeDriver.close();
+		
+		 
+				}
+				}
+			
+		
+		
+		
+		
+		
+		
+		
 	
-
-driver.close();
-
-
-}
-}
---
+++ DecemebeTddProject/src/test/java/upstest/UpsTest.java
@@ -0,0 +1,11 @@
+package upstest;
+
+import org.testng.annotations.BeforeMethod;
+
+public class UpsTest {
+
+	@BeforeMethod
+	void chromeDriverMethod() {
+	}
+
+}
