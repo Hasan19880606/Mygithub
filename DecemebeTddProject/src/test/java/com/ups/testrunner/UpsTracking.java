40ca19ece712a33733d2970020a1bce19d781d79
Shoeb <Shoebmah91@gmail.com>
2020-12-10 20:35:33 -0500

    Aftermethod
    Signed-off-by: Shoeb <Shoebmah91@gmail.com>

--- DecemebeTddProject/src/test/java/com/ups/testrunner/UpsTracking.java
+++ DecemebeTddProject/src/test/java/com/ups/testrunner/UpsTracking.java
@@ -34,14 +34,37 @@ public class UpsTracking {
 		Thread.sleep(2000);
 		loginButton.click();
 		Thread.sleep(2000);
+		chromeDriver.get("https://www.ups.com/track?loc=en_US&requester=ST/");
+		chromeDriver.manage().window().maximize();
+		chromeDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		WebElement tracking=chromeDriver.findElement(By.xpath("//*[@id=\"stApp_trackingNumber\"]"));
+		tracking.sendKeys("1234");
+		Thread.sleep(2000);
+		WebElement Track=chromeDriver.findElement(By.xpath("//*[@id=\"stApp_btnTrack\"]"));
+		Track.click();
+		WebElement Error=chromeDriver.findElement(By.xpath("//*[@id=\"stApp_statusErrorText\"]"));
+		if(Error.isDisplayed())
+				 {
+			System.out.println("Pass");}
+			else {System.out.println("fail");
+		}
+		
+		
+		Thread.sleep(2000);
+		chromeDriver.get("https://www.ups.com/ship/guided/origin?tx=6245488780265810&loc=en_US");
 		chromeDriver.close();
+		String msg="huimb";
+		WebElement Errors=chromeDriver.findElement(By.xpath("//*[@id=\"stApp_error_alert_list2\"]"));
+		msg=Errors.getText();
+		
+		
 		
 		 
 				}
 				}
 			
 		
-		
+
 		
 		
 		
--- DecemebeTddProject/src/test/java/upstest/UpsTest.java
+++ DecemebeTddProject/src/test/java/upstest/UpsTest.java
@@ -1,11 +1,29 @@
 package upstest;
 
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class UpsTest {
 
 	@BeforeMethod
 	void chromeDriverMethod() {
+		
+		
 	}
+	@Test
+	void noTest() {}
+	@Test
+	void loginTest() {}
+	@Test
+	void TrackingTest() {
+		
+		
 
+	}
+	
 }
